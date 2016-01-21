ab86553c6b0d5e6920489c4239e122a35554091c
Haley Hager <haleyerinhager@Haleys-MBP.att.net>
2016-01-20 22:12:02 -0500

    week 2

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,15 @@
 # haley hager
 
-print('Hello!')
+print('Hello! Choose your language:')
 
 print('For Latin, press 1.')
 print('For French, press 2.')
 print('For German, press 3.')
-
-if Latin == '1':
-print('Salve!')
-
-if French == '2':
-print('Salut!')
-
-if German == '3':
-print('Hallo!')
+    
+Language = 'English'
+if Language == '1':
+    print('Salve')
+if Language == '2':
+    print('Salut')
+if Language == '3':
+    print('Hallo')

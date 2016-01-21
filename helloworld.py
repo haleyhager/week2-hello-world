eabc5d57dd19025705463f3f7fdc700b767cb1bb
Haley Hager <haleyerinhager@Haleys-MBP.att.net>
2016-01-20 19:00:45 -0500

    week 2

--- helloworld.py
+++ helloworld.py
@@ -6,16 +6,11 @@ print('For Latin, press 1.')
 print('For French, press 2.')
 print('For German, press 3.')
 
-salve = '1'  #store values in the variables
-salut = '2'
-hallo = '3'
+if Latin == '1'
+print('Salve!')
 
+if French == '2'
+print('Salut!')
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
-
-# make sure that your code contains comments explaining your logic!
-
+if German == '3'
+print('Hallo!')
